bbb2a6e10603ddbba63566f1d44410bd649a9c42
kuldeep7688 <7688kuldeepsingh@gmail.com>
2025-12-24 16:51:26 -0500

    added a small change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and have fun too much fun!!
+          Learn React and have fun!!
         </a>
       </header>
     </div>
